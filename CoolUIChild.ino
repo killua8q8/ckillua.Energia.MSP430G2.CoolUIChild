b9d30638751d9078ab709c74b88b8a87a967538e
C Killua <killua8q8@gmail.com>
2015-02-05 05:01:44 -0500

    Completed node assginment and added empty functions
    * Recoded node assigment within loop();
    + Added getTemp();
    + Added on();
    + Added off();
    % Changed packet structure
    - Removed/Commented old codes

--- CoolUIChild.ino
+++ CoolUIChild.ino
@@ -2,32 +2,93 @@
 #include <AIR430BoostFCC.h>
 #include <string.h>
 
-#define TYPE "fan"
+#define ADDRESS_PARENT 0x01
 
 struct sPacket
 {
+  uint8_t parent;
   uint8_t node;
-  uint8_t msg[59];
+  uint8_t msg[58];
 };
 struct sPacket rxPacket;
 struct sPacket txPacket;
-uint8_t ADDRESS_PAIR = 0x00;    // using 0x00 when pairing
-uint8_t ADDRESS_PARENT = 0x00;  // assigned when paried
-uint8_t ADDRESS_LOCAL = 0x5F;   // using 0x5F as base when paring, changes when paried with assigned value
-boolean firstInitialized = false;
+/* child type as Node: VENT -> 0x60; FAN -> 0x70; BLIND -> 0x80 */
+uint8_t ADDRESS_LOCAL = 0x60;
+boolean initialized = false;
 
 void setup()
 {
   Serial.begin(9600);
-  firstInitializing();
+  pinMode(RED_LED, OUTPUT);
+  digitalWrite(RED_LED, LOW);
+//  firstInitializing();
+  memset(txPacket.msg, 0, sizeof(txPacket.msg));
+  txPacket.node = ADDRESS_LOCAL;
+  Radio.begin(ADDRESS_LOCAL, CHANNEL_1, POWER_MAX);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  while (Radio.receiverOn((unsigned char*)&rxPacket, sizeof(rxPacket), 0) > 0) {
+//    String message = (char*) rxPacket.msg;
+    
+    if (initialized && rxPacket.node == ADDRESS_LOCAL) {
+      Serial.println((char*)rxPacket.msg);
+//      Serial.println(rxPacket.node);
+//      Serial.println(message);
+    } else if (!initialized && !strcmp((char*)rxPacket.msg, "PAIR")) {
+//      Serial.println(rxPacket.parent);
+//      Serial.println(rxPacket.node);
+//      Serial.println(message);
+      ADDRESS_LOCAL = rxPacket.node;
+      strcpy((char*)txPacket.msg, "ACK");
+      delay(1000);
+      Radio.transmit(ADDRESS_PARENT, (unsigned char*)&txPacket, sizeof(txPacket));
+      digitalWrite(RED_LED, HIGH);
+      initialized = true;
+      while(Radio.busy()){}
+      Radio.end();
+      while(Radio.busy()){}
+      Radio.begin(ADDRESS_LOCAL, CHANNEL_1, POWER_MAX);
+      txPacket.node = ADDRESS_LOCAL;
+    } 
+    
+//    Serial.println("end");
+  }
+}
+
+uint8_t getTemp() {
+  return 0;
+}
+
+void on() {
+ 
+}
+
+void off() {
   
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/************** OLD CODES **************
 void firstInitializing() {
   Radio.begin(ADDRESS_LOCAL, CHANNEL_1, POWER_MAX);
   txPacket.node = ADDRESS_LOCAL;
@@ -35,7 +96,7 @@ void firstInitializing() {
   while(!firstInitialized) {
     while (Radio.receiverOn((unsigned char*)&rxPacket, sizeof(rxPacket), 0) <= 0) {}
     char* msg = (char*)rxPacket.msg;
-    char smsg[6][10];
+    char smsg[4][10];
     uint8_t x = 0, y = 0;
     for (int i = 0; i < strlen(msg); i++) {
       if (msg[i] != ' ') {
@@ -49,17 +110,31 @@ void firstInitializing() {
     }
     smsg[x][y] = '\0';
     ADDRESS_PARENT = rxPacket.node;
-    ADDRESS_LOCAL = atoi(smsg[3]);
+    ADDRESS_LOCAL = atoi(smsg[1]);
     txPacket.node = ADDRESS_LOCAL;
-    delay(500);
+    delay(2000);
     Radio.transmit(ADDRESS_PAIR, (unsigned char*)&txPacket, sizeof(txPacket));
-    firstInitialized = true;    
+    Radio.setAddress(ADDRESS_LOCAL);
+    delay(500);
+    if (testConnection()) {
+      firstInitialized = true;
+      digitalWrite(RED_LED, HIGH);
+    }
   }
-  Radio.setAddress(ADDRESS_LOCAL);
   Serial.print("connection made to node: ");
   Serial.println(ADDRESS_PARENT);
 }
 
-uint8_t getTemp() {
-  return 0;
+boolean testConnection() {
+//  Serial.println(ADDRESS_LOCAL);
+  while (Radio.receiverOn((unsigned char*)&rxPacket, sizeof(rxPacket), 0) <= 0) {}
+//  Serial.println(rxPacket.node);
+  if (rxPacket.node = ADDRESS_PARENT) {
+//     delay(2000);
+    txPacket.node = 0x90;
+    Radio.transmit(ADDRESS_PARENT, (unsigned char*)&txPacket, sizeof(txPacket));
+    return true;
+  }
+  return false;
 }
+***************************************/
